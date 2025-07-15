3d3a2322c0b3a80828173e9ef68c86058256c01a
Alan Franklin <167710977+Alan0Franklin@users.noreply.github.com>
2025-07-15 19:19:18 -0300

    Update imovel.hpp
    Comentários adicionados.

--- imovel.hpp
+++ imovel.hpp
@@ -12,20 +12,25 @@ private:
     std::string endereco;
     double lat, lng, preco;
 public:
+    // Contrutores de Imóvel:
     Imovel();
     Imovel(std::string tipo, int proprietarioId, std::string endereco, double lat, double lng, double preco);
-    int getId();
+    
+    // Atribuem valores aos atributos do Imóvel:
     void setTipo(std::string tipo);
     void setIdDoProprietario(int proprietarioId);
     void setEndereco(std::string endereco);
     void setLatitude(double lat);
     void setLongitude(double lng);
     void setPreco(double preco);
+
+    // Obtem os atributos do Imóvel:
+    int getId();
     std::string getTipo();
     int getIdDoProprietario();
     std::string getEndereco();
-    double getLatitude();
-    double getLongitude();
+    double getLatitude() const;
+    double getLongitude() const;
     double getPreco();
     void exibirInformacoes();
 };
