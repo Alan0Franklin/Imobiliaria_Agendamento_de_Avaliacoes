b8135ffb0daa8c0aec117138e8d2d02585289641
Alan Franklin <167710977+Alan0Franklin@users.noreply.github.com>
2025-07-15 19:09:29 -0300

    Update util.hpp
    Reprogramado com ponteiros inteligentes + Horário de visita dos imóveis agora leva em conta o imóvel mais próximo do último lugar visitado por esse avaliador, ou da moradia do avaliador se ele não visitou nenhum imóvel em algum horário anterior + Comentários adicionados.

--- util.hpp
+++ util.hpp
@@ -1,16 +1,25 @@
 #ifndef UTIL_HPP
 #define UTIL_HPP
 
+#include <memory>
 #include <vector>
+#include <list>
 #include <map>
 #include "corretor.hpp"
 #include "imovel.hpp"
 
+// Estrutura comparadora que permite a ordenação dos corretores em um std::map
 struct PessoaIDComparator {
-    bool operator()(const Corretor* a, const Corretor* b) const;
+    bool operator()(const std::shared_ptr<Corretor> a, const std::shared_ptr<Corretor> b) const;
 };
 
+// Função haversine para calcular a distância entre duas coordenadas na superfície do globo
 double haversine(double lat1, double lng1, double lat2, double lng2);
-void imprimirAgenda(std::map<Corretor*, std::vector<Imovel*>, PessoaIDComparator>& Agenda);
+
+// Função responsável por gerar a agenda de visitas dos avaliadores
+void imprimirAgenda(std::map<std::shared_ptr<Corretor>, std::vector<std::shared_ptr<Imovel>>, PessoaIDComparator>& Agenda);
+
+// Função que retorna o imóvel da lista de imóveis, que aquele avaliador deve visitar, mais próximo da posição atual do avaliador
+std::list<std::shared_ptr<Imovel>>::iterator imovelmaisproximo(double lat, double lng, std::list<std::shared_ptr<Imovel>>& lista_imoveis);
 
 #endif
