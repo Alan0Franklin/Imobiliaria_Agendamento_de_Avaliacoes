9d772bf7080e24286375ebabd41e335a6db81fde
Alan Franklin <167710977+Alan0Franklin@users.noreply.github.com>
2025-07-15 19:23:01 -0300

    Update corretor.hpp
    Comentários adicionados.

--- corretor.hpp
+++ corretor.hpp
@@ -10,11 +10,16 @@ private:
     bool avaliador;
     double lat, lng;
 public:
+    // Contrutores de Corretor:
     Corretor();
     Corretor(std::string nome, std::string telefone, int avaliador, double lat, double lng);
+    
+    // Atribuem valores aos atributos do Corretor:
     void setAvaliador(int avaliador);
     void setLatitude(double lat);
     void setLongitude(double lng);
+    
+    // Obtem os atributos do Corretor:
     bool getAvaliador();
     double getLatitude();
     double getLongitude();
